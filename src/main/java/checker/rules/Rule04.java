6edcc6c6e3980dd3693053d897b102909e48f2b1
natan <nikolic.nt@gmail.com>
2022-06-03 18:47:39 +0200

    Rule 4 (Required query parts) done

--- src/main/java/checker/rules/Rule04.java
+++ src/main/java/checker/rules/Rule04.java
@@ -1,7 +1,64 @@
 package checker.rules;
 
 import checker.rules.neon.AbstractRule;
+import execute.data.RunData;
+import execute.data.Statement;
 
 public class Rule04 extends AbstractRule {
 
+    @Override
+    public boolean checkRule(Object data) {
+        RunData runData = (RunData) data;
+//        Query q = (Query) data;
+        boolean noSelect = true;
+        for(Statement statement : runData.getStatementList()){
+            if(statement.getKeyword().equalsIgnoreCase("select")){
+                noSelect = false;
+            }
+        }
+        if(noSelect){
+            generateErrorSuggestion("select");
+            return false;
+        }
+
+        boolean groupBy = false;
+        boolean having = false;
+
+        for(Statement statement : runData.getStatementList()){
+            if(statement.getKeyword().equalsIgnoreCase("group by")){
+                groupBy = true;
+            }
+            if(statement.getKeyword().equalsIgnoreCase("having")){
+                having = true;
+            }
+        }
+
+        if(!groupBy && having){
+            generateErrorSuggestion("group by");
+            return false;
+        }
+
+
+        for(Statement statement : runData.getStatementList()){
+            String text = statement.getText();
+            boolean isJoin = text.toLowerCase().contains("join");
+            if(!statement.getKeyword().equalsIgnoreCase("from") && isJoin){
+                generateErrorSuggestion("join in from");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public void generateErrorSuggestion(Object data) {
+        String s = (String) data;
+        String str = getError();
+        str = str.replace("%1", s);
+        setErrorMsg(str);
+        str = getSuggestion();
+        str = str.replace("%1", s);
+        setSuggestionMsg(str);
+    }
 }
